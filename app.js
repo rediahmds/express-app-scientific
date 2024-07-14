7f535be836e240c4ae45068861f315956367d425
Redi Ahmad S <90599494+rediahmds@users.noreply.github.com>
2024-07-14 23:28:06 +0700

    update port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { retrieveHostInfo } = require('./utils/sysinfos');
 const { createBody } = require('./utils/body');
 const app = express();
-const port = process.env.PORT | 80;
+const port = process.env.PORT | 3000;
 
 const { hostName, ipv4 } = retrieveHostInfo();
 
