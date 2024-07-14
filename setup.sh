7717f04da7f9b0c1c27c55f7737c685fd93923f7
Redi Ahmad S <90599494+rediahmds@users.noreply.github.com>
2024-07-14 23:23:24 +0700

    source nvm script directly

--- setup.sh
+++ setup.sh
@@ -15,9 +15,10 @@ fi
 echo "Installing nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-# 3. Source the ~/.bashrc
-echo "Sourcing ~/.bashrc..."
-source ~/.bashrc
+# 3. Source the nvm script directly
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # 4. Check if nvm is installed successfully
 if ! command -v nvm &> /dev/null; then
