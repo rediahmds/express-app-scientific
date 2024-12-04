d269fa4c32b5ba45a515d5131bc81a0648472f19
Redi Ahmad S <90599494+rediahmds@users.noreply.github.com>
2024-12-04 11:07:05 +0700

    set default port number
    http 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { retrieveHostInfo } = require('./utils/sysinfos');
 const { createBody } = require('./utils/body');
 const app = express();
-const port = process.env.PORT | 8080;
+const port = process.env.PORT | 80;
 
 const { hostName, ipv4 } = retrieveHostInfo();
 
